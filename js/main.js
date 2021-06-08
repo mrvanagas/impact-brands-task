b7e39a0c1e7ab4b996f829e11f6502c7ff493f32
mrvanagas <lukonelis1@gmail.com>
2021-06-08 13:13:46 +0300

    feat: slider v1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
-//navbar
+//navbar logic
 let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
 navBarToggle.addEventListener('click', function () {
   mainNav.classList.toggle('active');
 });
+
+//auto slider logic
+
+let counter = 1;
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 3){
+        counter = 1;
+    }
+}, 5000);
